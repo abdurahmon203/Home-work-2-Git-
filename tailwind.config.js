9f65e0bcf3ead2ac9692f9cc298c7ddf89a67c5c
Abdurahmon <nozimovislom101@gmail.com>
2025-03-23 11:58:15 +0300

    Kadm

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+
+      'xs': '360px',   // Phones
+      'md': '670px',   // ipad
+      'lg': '1280px',  // Desktops
+
+  },
+  },
+  plugins: [],
+}
+
